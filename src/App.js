94f422f0d17912021494a4b42337aa51c422786c
Ryland Taniguchi <ryland0000@gmail.com>
2022-02-27 04:38:59 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState, Fragment, useEffect } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [resourceType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, [resourceType]);
+
+  return (
+    <>
+      <div>
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("users")}>Users</button>
+        <button onClick={() => setResourceType("comments")}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+      {items.map((item) => {
+        return <pre>{JSON.stringify(item)}</pre>;
+      })}
+    </>
+  );
+}
